2c8bb74f07c2581cbb7861671f5d150ce079e8fd
Patrick A <akolo@hotmail.com>
2020-03-10 14:42:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,7 @@
 require 'pry'
 
 def my_own_map(element1, element2)
-  yield ((element1 * element2))
+  yield 
   
 end 
-my_own_map([1,2,3,-9], -1) {|n| * -1}
-end 
+my_own_map(element1.map  {|n| * element2}
