e20b5d658b53d90f573df9fc2ec545c70e9c78e8
Patrick A <akolo@hotmail.com>
2020-03-10 15:28:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,5 +12,13 @@ def map(element1)
 end 
 
 def reduce
+  new = starting_point
+  i = 0
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
+end
   
 end
