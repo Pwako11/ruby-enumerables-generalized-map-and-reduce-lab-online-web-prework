f6237f4f71620d1ed8a92d17ddc14ad3f6ad9db8
Patrick A <akolo@hotmail.com>
2020-03-10 15:29:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,14 +11,12 @@ def map(element1)
   new
 end 
 
-def reduce
+def reduce(source_array, starting_point=0)
   new = starting_point
   i = 0
   while i < source_array.length do
-    new += source_array[i]
+    new +=(yield (source_array[i]))
     i += 1
   end
-  return new
-end
-  
+  new
 end
