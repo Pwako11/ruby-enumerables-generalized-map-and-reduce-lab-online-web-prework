0b115b6f8473a901cc190e42bc9c09e154b5337d
Patrick A <akolo@hotmail.com>
2020-03-10 15:06:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,16 @@
 # Your Code Here
 require 'pry'
 
-def my_own_map(element1, element2)
-  yield (element1 * element2)
-  
+def my_own_map(element1)
+  new = []
+  i = 0 
+  while i <  element1.length do 
+    new.push(yield(element1[i]))
+    i += 1 
+  end 
+  new
 end 
+
 my_own_map(element1).map  do |n| 
-  n * element2
+  n * -1
 end 
